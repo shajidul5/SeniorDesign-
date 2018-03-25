2cf767a96a72a29065b6bac71d58a516d2423349
Armando <Armando024@live.com>
2018-03-25 00:38:11 -0400

    Enable car to move in the wider road

--- cars4/gui/car.py
+++ cars4/gui/car.py
@@ -6,12 +6,21 @@ class Car:
         self.h=h
         self.x=x
         self.y=y
-    
+        self.temp_sides=0
+        self.up_down=0
+       # self.first_time=True
+         #the multi used for gui
     def __str__(self):
         #for debuffing puposes
         temp="x=="+self.x+" y=="+self.y
         return temp
-    
+    def get_x1(self):
+        return self.x*(30)+(self.up_down*5)
+    def get_y1(self):
+        #if self.first_time:
+         #   return self.y*30+(self.temp_sides)
+        #print(self.y*30+(self.temp_sides*5))
+        return self.y*30+(self.temp_sides*5)
     def get_x(self): 
         return self.x
     
@@ -19,19 +28,48 @@ class Car:
         return self.y
     
     def m_r(self): #move right
-        self.x+=1
+        if (self.up_down is 6):
+            self.x+=1
+            self.up_down=0
+        self.up_down+=1
         return
-    
+    def dec_up_down(self):
+        if (self.up_down is not 0 ):
+            self.up_down-=1
+        return
+    def inc_up_down(self):
+        if (self.up_down is not 4):
+            self.up_down+=1
+        return
     def m_l(self): #move left
-        self.x-=1
+        if (self.up_down is 0):
+            self.x-=1
+            self.up_down=6
+        self.up_down-=1
         return 
-
-    def m_u(self): #move up
-        self.y-=1
+    def dec_side(self):
+        if(self.temp_sides is not 0):
+            self.temp_sides-=1
+        return
+    def inc_side(self):
+        if(self.temp_sides is not 4):
+            self.temp_sides+=1
+    def m_u(self): #move real left
+        if (self.temp_sides is 0):
+            #print("chaging y")
+            self.y-=1
+            self.temp_sides=6
+        #print("chaging sides temp")
+        self.temp_sides-=1
         return
     
-    def m_d(self): #move down
-        self.y+=1
+    def m_d(self): #move real right
+       # print("going right") 
+        if (self.temp_sides is 6):
+            self.y+=1
+            self.temp_sides=0
+       # self.first_time=False
+        self.temp_sides+=1
         return
 
     def get_w(self): #get the width
@@ -39,3 +77,14 @@ class Car:
 
     def get_h(self): #get the height
         return self.h 
+    
+  
+
+
+
+
+
+
+
+
+
--- cars4/gui/maze.py
+++ cars4/gui/maze.py
@@ -17,22 +17,7 @@ class Maze:
         self.w=w
         self.h=h
         self.maze=BaseMaze(self.w,self.h,complexity,density)
-        
-    def get_Maze(self):
-        return self.maze
-    def get_W(self):
-        return self.w
-    def get_H(self):
-        return self.h
-   # def get_Value(self,y,x):
-        #height=y x=width
-        #print("y=="+((str)(y)),end="")
-        #print(" x=="+((str)(x)))
-    #    return self.maze.get[y][x]
-    def get_cons(self):
-        return self.complexity
-    def get_dens(self):
-        return self.density
+         
     def get_event(self,event):
         if event.type == sc.KEYDOWN:
             if event.key==sc.K_RIGHT: #re-builds maze
@@ -44,22 +29,25 @@ class Maze:
             elif event.key==sc.K_DOWN: #increases density
                 self.car=Car(10,10,1,1)
                 self.maze.inc_den()   
-           #car ill make it more OOP late
+            #adding new part to the code
             if event.key==sc.K_a and self.maze.get_Value(int(self.car.get_x()),int(self.car.get_y())-1)==0: #UP
                 self.car.m_u()
-            if event.key==sc.K_d and self.maze.get_Value(int(self.car.get_x()),int(self.car.get_y())+1)==0: #DOWN
+            elif  event.key==sc.K_a: 
+                self.car.dec_side()
+            if (event.key==sc.K_d and self.maze.get_Value(int(self.car.get_x()),int(self.car.get_y())+1)==0): #right
                 self.car.m_d()
+            elif  event.key==sc.K_d:
+                self.car.inc_side()
             if event.key==sc.K_w and self.maze.get_Value(int(self.car.get_x())-1,int(self.car.get_y()))==0: #Left
                 self.car.m_l()
+            elif event.key==sc.K_w:
+                self.car.dec_up_down()
+            
             if event.key==sc.K_s and self.maze.get_Value(int(self.car.get_x())+1,int(self.car.get_y()))==0: #right
                 self.car.m_r()
+            elif event.key==sc.K_s:
+                self.car.inc_up_down()
 
-    def inc_con(self):
-        if self.complexity<=1:
-            self.complexity+=.02
-    def inc_dens(self):
-        if self.density<=1:
-            self.density+=.02
     def update(self,screen,dt): 
         self.draw(screen)
     def draw(self,screen): #actual drawing goes here
@@ -68,8 +56,8 @@ class Maze:
         x1=0
         y1=0
         Cw=30 #cell width
-        for a in range(0,self.get_W()):
-            for b in range(0,self.get_H()):
+        for a in range(0,self.maze.g_w()):
+            for b in range(0,self.maze.g_h()):
                 if self.maze.get_Value(b,a)==0: #road
                     sc.draw.rect(screen,(128,128,128),sc.Rect(y1,x1,Cw,Cw))
                     sc.draw.rect(screen,(128,128,128),sc.Rect(y1,x1,Cw,Cw))
@@ -77,10 +65,11 @@ class Maze:
             y1=y1+30
             x1=0
              
-        z1=int(self.car.get_x())*30
-        z2=int(self.car.get_y())*30
-        sc.draw.rect(screen,(0,0,255),sc.Rect(z2,z1,self.car.get_h(),self.car.get_w()))
+        #z1=int(self.car.get_x())*30
+        #z2=int(self.car.get_y())*30
+        #sc.draw.rect(screen,(0,0,255),sc.Rect(z2,z1,self.car.get_h(),self.car.get_w()))
 
+        sc.draw.rect(screen,(0,0,255),sc.Rect(self.car.get_y1(),self.car.get_x1(),self.car.get_h(),self.car.get_w()))
 
         screen.blit(self.text,(0,455))
         screen.blit(self.text1,(0,505))
@@ -92,13 +81,6 @@ class Maze:
     def cleanup(self):
         #createclean up
         pass
-  #    def
-#0=empty space
-#1=wall
-#a=Maze()
-#b=a.get_Maze() 
-#for b in a.get_Maze():
- #   print(b)
 
 
 
